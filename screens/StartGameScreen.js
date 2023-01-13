dbc4ccd69a97a87fb9ae69e80d930c38b8e9083e
jarroyo28 <johnarroyo45@gmail.com>
2023-01-12 21:51:19 -0600

    created the initial screens for the app, and added prettier to the project

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { TextInput, View } from "react-native";
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      <TextInput />
+      <Text>TWO BUTTONS HERE</Text>
+    </View>
+  );
+};
+
+export default StartGameScreen;
