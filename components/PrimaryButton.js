7d827c99ac1c78d2e75c1c7d7ea35183bcb26ea8
jarroyo28 <johnarroyo45@gmail.com>
2023-01-16 21:08:16 -0600

    created a PrimaryButton component that will be used to create a custom button

---
+++ components/PrimaryButton.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const PrimaryButton = (props) => {
+  return (
+    <View>
+      <Text>{props.children}</Text>
+    </View>
+  );
+};
+
+export default PrimaryButton;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,12 @@
 import { TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
   return (
     <View>
       <TextInput />
-      <Text>TWO BUTTONS HERE</Text>
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 };
