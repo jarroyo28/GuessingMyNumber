76a54261f62e06cee54af82fb2ae8b036ce5ada6
jarroyo28 <johnarroyo45@gmail.com>
2023-01-23 22:54:10 -0600

    created the functionality for the + and - buttons in the game screen component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,16 +1,39 @@
 import { useState } from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, Alert } from "react-native";
 import Title from "../components/ui/Title";
 import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
 const generateRandomNumber = (min, max) => {
   const randomNumber = Math.floor(Math.random() * (max - min)) + min;
   return randomNumber;
 };
 
+let minBoundary = 1;
+let maxBoundary = 100;
+
 const GameScreen = (props) => {
-  const intialGuess = generateRandomNumber(1, 100);
+  const intialGuess = generateRandomNumber(minBoundary, maxBoundary);
   const [guessedNumber, setGuessedNumber] = useState(intialGuess);
+
+  const handleNewGuess = (direction) => {
+    if (
+      (direction === "lower" && guessedNumber < props.userNumber) ||
+      (direction === "higher" && guessedNumber > props.userNumber)
+    ) {
+      Alert.alert("Don't lie now...", "You and I both know you are...", [
+        { text: "Okay", style: "cancel" },
+      ]);
+      return;
+    }
+    if (direction === "lower") {
+      maxBoundary = guessedNumber;
+    } else {
+      minBoundary = guessedNumber + 1;
+    }
+    const newRandomNumber = generateRandomNumber(minBoundary, maxBoundary);
+    setGuessedNumber(newRandomNumber);
+  };
 
   return (
     <View style={styles.gameScreenContainer}>
@@ -18,6 +41,14 @@ const GameScreen = (props) => {
       <NumberContainer>{guessedNumber}</NumberContainer>
       <View>
         <Text>Higher or Lower?</Text>
+        <View>
+          <PrimaryButton onPress={() => handleNewGuess("higher")}>
+            +
+          </PrimaryButton>
+          <PrimaryButton onPress={() => handleNewGuess("lower")}>
+            -
+          </PrimaryButton>
+        </View>
       </View>
       <View></View>
     </View>
